ade64c4845bab632c3855026886ea40a6370e107
Farah Qandalaft <farahqandalaft@optimumpartners.co>
2022-08-21 07:54:41 -0400

    Css : flex practice fix

--- flex/practice3/main.js
+++ flex/practice3/main.js
@@ -1,9 +1,13 @@
 const sidebarBtn = document.getElementById("sidebar-btn");
 const sidebar = document.getElementById("sidebar");
 const main = document.getElementById("main-container");
+const searchLargeScreen = document.getElementById("search-lg");
+const searchSmallScreen = document.getElementById("search-sm");
 
 const showSidebar = () => {
   if (screen.width > 600) {
+    searchSmallScreen.className = "hide-side";
+    searchLargeScreen.className = "search";
     if (sidebar.className == "hide-side") {
       sidebar.className = "show-side";
       main.style.width = "86vw";
