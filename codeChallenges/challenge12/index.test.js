e3f496182fe4b4298c2d00dcef23192316232522
Farah Qandalaft <farah@farah-qandalaft-pc.scoiumtech.local>
2022-09-08 11:06:51 +0300

    challenge 12 : second max

---
+++ codeChallenges/challenge12/index.test.js
@@ -0,0 +1,18 @@
+/*
+You have to create a function
+that takes an array of integer number and returns the
+second max in the array:
+Note : using sort native is not allowed
+Exanmples:
+secondMax([1,5, 2])== 2
+secondMax([-1, -5, 2])== -1
+*/
+const secondMax = function (array) {
+  // TODO
+  let arr = array;
+  let maxNumber = Math.max(...arr);
+  const indexOfmax = arr.indexOf(maxNumber);
+  arr.splice(indexOfmax, 1);
+  maxNumber = Math.max(...arr);
+  return maxNumber;
+};
